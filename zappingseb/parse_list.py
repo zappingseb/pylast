60f429df9538af649184d3bee1a81b9938611176
zappingseb <sebastian@mail-wolf.de>
2017-03-02 00:40:03 +0100

    Wrote a functional deparser for Robert Luis MixCloud Playlists

--- zappingseb/parse_list.py
+++ zappingseb/parse_list.py
@@ -4,6 +4,7 @@ import pylast
 import os
 import time
 import re
+import pandas as pd
 
 def assertEqual(first, second, msg=None):
     """Fail if the two objects are unequal as determined by the '=='
@@ -27,14 +28,46 @@ def get_secret_dict(secrets_file="../tests/test_pylast.yaml"):
             return {}
     return doc
 
-def deparse_txt(filename="./test_list.txt"):
+def deparse_txt(filename="./test_list.txt", exceptions=None):
+    if not exceptions:
+        exceptions = ["Remix", "Reprise", "Edit"]
+
+    flatten = lambda l: [item for sublist in l for item in sublist]
+
     if not os.path.isfile(filename):
         raise LookupError("File {:s} could not be found".format(filename))
     else:
         with open(filename) as file_input:
-            file_lines = file_input.read().split("\n")
-            print([re.sub("\)(\s)[^\-]{1,30}]",
-                           file_line,"\n") for file_line in file_lines])
+            list_of_artist_track = file_input.read().split("\n")
+
+            for splitter in ["-","–"]:
+
+                list_of_artist_track = flatten([file_line.split(splitter) for file_line in list_of_artist_track])
+
+
+            for entry in exceptions:
+                pattern = "".join(["(?:", entry, ")(\))"])
+                out_pat = "".join([entry,"|)"])
+                list_of_artist_track = [re.sub(pattern, out_pat, entry) for entry in list_of_artist_track]
+
+            print(list_of_artist_track)
+            print("-----")
+
+            list_of_artist_track = flatten([re.split("(?<!\|)(\))", entry) for entry in list_of_artist_track])
+
+
+            list_of_artist_track = [element for element in list_of_artist_track if element != ")"]
+
+
+            list_of_artist_track = [
+                [list_of_artist_track[i].strip().strip("|"),
+                 "".join(list_of_artist_track[i+1].strip().split("(")[
+                     0:list_of_artist_track[i+1].count("(")
+                 ]).strip().replace("|","").strip(")")]
+                   for i in range(0, len(list_of_artist_track)-1, 2)]
+
+            list_of_artist_track = [var for var in list_of_artist_track if len(var)>0]
+            return(pd.DataFrame(list_of_artist_track))
 
 
 if __name__ == "__main__":
